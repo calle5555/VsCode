512fad302758259dc60699db8de9dd7f7c72a2cc
Carl-William Lindkvist <lindkvistcalle@gmail.com>
2021-11-09 11:45:04 +0100

    Co-authored-by: lulletho <lulletho@users.noreply.github.com>

---
+++ OOP/Lab1.c
@@ -0,0 +1,48 @@
+// Kommentar yay https://code.visualstudio.com/docs/languages/cpp
+
+typedef struct
+{
+    char name[50];
+    char code[50];
+    int points;
+    int year;
+    int period;
+    struct Teachers* teacher;
+    struct Students* student;
+
+
+}Courses;
+
+typedef struct
+{
+    char name[50];
+    char email[50];
+    int phone;
+    int year;
+    int id;
+    struct Courses* courses;
+
+}Students;
+
+typedef struct
+{
+
+    char name[50];
+    char email[50];
+    int phone;
+    int office_num;
+    struct Courses* courses;
+
+}Teachers;
+
+
+
+
+
+
+typedef struct
+{
+    struct Students* student;
+    struct Courses* course;
+    struct Teachers* teacher;
+}Minibb;
