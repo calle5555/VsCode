4d1868c7ebefb14dc0d1caf28fd673c1e7136eb5
calle5555 <84193411+calle5555@users.noreply.github.com>
2021-11-30 11:04:47 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Program Files/VSCode/VsCode
-# Build directory: C:/Program Files/VSCode/VsCode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
